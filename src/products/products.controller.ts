c8a08aa9bf7085b3b995c28258db1b2bc0bc25e4
lksad <it20605902@my.sliit.lk>
2023-06-24 13:56:48 +0530

    Comple rest of the things of adding products endpoint

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,19 @@
-import { Controller, Post } from "@nestjs/common";
+import { Controller, Post, Body } from "@nestjs/common";
+import { ProductService } from "./products.service";
 
 @Controller('Products')
 export class ProductController {
+    constructor(private readonly productService: ProductService) {}
     @Post()
-    addProduct(): any{}
+    addProduct(
+        @Body('title') proTitle: string,
+        @Body('description') proDesc: string,
+        @Body('price') proPrice: number,
+    ) {
+       const generatedId =  this.productService.insertProduct(
+        proTitle, 
+        proDesc, 
+        proPrice); 
+       return {id: generatedId}
+    }
 }
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -6,8 +6,10 @@ export class ProductService {
     products: Product[] = [];
     
     insertProduct(title: string, desc: string, price: number) {
-        const newProduct = new Product(new Date().toString(), title, desc, price);
+        const prodId = new Date().toString();
+        const newProduct = new Product(prodId, title, desc, price);
         this.products.push(newProduct);
+        return prodId;
 
     }
 
